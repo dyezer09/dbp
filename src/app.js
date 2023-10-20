1e2c88fca750b17a9e2c24ac50ad73f4069b2e6d
adj-w-hp <adjyez@yahoo.com>
2023-10-20 20:50:17 +0300

    add: env

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import path from "path";
 import bodyParser from 'body-parser';
 import fs from 'fs';
 import format from "date-format";
+import "dotenv/config";
+
 //import { constants } from "fs";
 const __dirname = path.resolve();
 let app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-let text = fs.readFileSync('hz.txt', 'utf8');
+let text = fs.readFileSync('src/hz.txt', 'utf8');
 let upBd,downBd;
 
 
@@ -15,19 +17,21 @@ app.post("/",function(req,res){
     let date = format.asString('dd-mm-yy', new Date());
     let time  = format.asString('hh:mm', new Date());
     let str = date + ' ' + time +  ': ' + req.body.upBd + '  |  ' + req.body.downBd + '\n';
-    fs.appendFileSync("hz.txt",str);
+    fs.appendFileSync("src/hz.txt",str);
     res.send(str);
 });
 
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + `/index.html`);
+    res.sendFile(__dirname + `/src/index.html`);
 });
 
 app.get("/all",function(req,res){
-     res.sendFile(__dirname + `/hz.txt`);
+     res.sendFile(__dirname + `/src/hz.txt`);
  });
 
-app.listen(5500,()=>{
-    console.log("start");
+ console.log(process.env.APP_PATH);
+const port = process.env.APP_PORT
+app.listen(port,()=>{
+    console.log("runnung:" + port);
 });
